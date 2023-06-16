1c69de440506ea08f1a9afdb96e8257f6562c01a
DanSun <daniel.suomin@gmail.com>
2023-06-16 17:06:56 +0300

    first commit

---
+++ gui.py
@@ -0,0 +1,58 @@
+import functions
+import PySimpleGUI as sg
+
+lable = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_b = sg.Button('Add')
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_b = sg.Button("Edit")
+compile_b = sg.Button('Complete')
+exit_b = sg.Button('Exit')
+
+window = sg.Window("My To-do project",
+                   layout=[[lable],
+                           [input_box, add_b], [list_box, edit_b, compile_b],
+                           [exit_b]], font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except ImportError:
+                sg.popup("Please select an item first.")
+
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case 'Exit':
+            break
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case sg.WIN_CLOSED:
+            break
+
+window.close()
